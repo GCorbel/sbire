3578a7b5e7c4b7852cecd0d090f088cdde26a718
Julien Blanchard <julien@sideburns.eu>
2013-11-01 13:17:16 +0100

    Use a different notification tool given the OS

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -1,5 +1,16 @@
 class Notifier
   def self.call(message)
-    system("notify-send '#{message}'")
+    system("#{command} '#{message}'")
+  end
+
+  def self.command
+    case RUBY_PLATFORM
+    when /darwin/
+      'terminal-notifier -sender com.apple.Terminal -message'
+    when /linux/
+      'notify-send'
+    when /win32/
+      raise NotImplementedError
+    end
   end
 end
