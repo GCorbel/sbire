d69e0d5a9bc188dd8c95eeaa2855b05bba3f2105
Julien Blanchard <julien@sideburns.eu>
2013-11-01 15:50:23 +0100

    Add ExternalTools class

---
+++ lib/external_tools.rb
@@ -0,0 +1,36 @@
+class ExternalTools
+
+  def self.notifier
+    case RUBY_PLATFORM
+    when /darwin/
+      'terminal-notifier -sende com.apple.Terminal -message'
+    when /linux/
+      'notify-send'
+    when /win32/
+      raise NotImplementedError
+    end
+  end
+
+  def self.recorder(path)
+    case RUBY_PLATFORM
+    when /darwin/
+      "sox -d #{path} >/dev/null 2>&1"
+    when /linux/
+      "ffmpeg -loglevel panic -f alsa -ac 2 -i pulse -y #{path} -r 22050 >/dev/null 2>&1"
+    when /win32/
+      raise NotImplementedError
+    end
+  end
+
+  def self.recorder_name
+    case RUBY_PLATFORM
+    when /darwin/
+      "sox"
+    when /linux/
+      "ffmpeg"
+    when /win32/
+      raise NotImplementedError
+    end
+  end
+
+end
