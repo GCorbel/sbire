d6383e836fdcfaaf1fbdc4e1c1a6bcff542839e2
Julien Blanchard <julien@sideburns.eu>
2013-11-01 16:01:04 +0100

    Pass message directly to external notifier

--- lib/external_tools.rb
+++ lib/external_tools.rb
@@ -1,11 +1,11 @@
 class ExternalTools
 
-  def self.notifier
+  def self.notifier(message)
     case RUBY_PLATFORM
     when /darwin/
-      'terminal-notifier -sende com.apple.Terminal -message'
+      "terminal-notifier -sende com.apple.Terminal -message #{message}"
     when /linux/
-      'notify-send'
+      "notify-send '#{message}'"
     when /win32/
       raise NotImplementedError
     end
@@ -14,7 +14,7 @@ class ExternalTools
   def self.recorder(path)
     case RUBY_PLATFORM
     when /darwin/
-      "sox -d #{path} >/dev/null 2>&1"
+      "sox -r 22050 -d #{path} >/dev/null 2>&1"
     when /linux/
       "ffmpeg -loglevel panic -f alsa -ac 2 -i pulse -y #{path} -r 22050 >/dev/null 2>&1"
     when /win32/
--- lib/notifier.rb
+++ lib/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier
   def self.call(message)
-    system("#{ExternalTools.notifier} '#{message}'")
+    system( ExternalTools.notifier(message) )
   end
 end
