7fa83373fbf8973d1b6f295f78285735df134a33
GCorbel <guirec.corbel@gmail.com>
2013-11-06 21:41:45 -0500

    correct the indentation

--- lib/sbire_config.rb
+++ lib/sbire_config.rb
@@ -1,38 +1,38 @@
 require 'yaml'
 
 class SbireConfig
-    def self.config_path
-      "#{base_directory}/config.yml"
-    end
+  def self.config_path
+    "#{base_directory}/config.yml"
+  end
 
-    def self.base_directory
-      "/home/dougui/.sbire"
-    end
+  def self.base_directory
+    "/home/dougui/.sbire"
+  end
 
-    def self.lang
-      config["lang"] ||= "en-US"
-    end
+  def self.lang
+    config["lang"] ||= "en-US"
+  end
 
-    def self.out_path
-      config["out_path"] ||= "#{base_directory}/out/"
-    end
+  def self.out_path
+    config["out_path"] ||= "#{base_directory}/out/"
+  end
 
-    def self.out_file
-      config["out_file"] ||= "#{out_path}.audiofile"
-    end
+  def self.out_file
+    config["out_file"] ||= "#{out_path}.audiofile"
+  end
 
-    def self.text_file
-      config["text_file"] ||= "#{base_directory}/text"
-    end
+  def self.text_file
+    config["text_file"] ||= "#{base_directory}/text"
+  end
 
-    def self.command_path
-      config["command_path"] ||= "#{base_directory}/commands.yml"
-    end
+  def self.command_path
+    config["command_path"] ||= "#{base_directory}/commands.yml"
+  end
 
-    private
+  private
 
-    def self.config
-      return @config if @config
-      @config = YAML.load_file(config_path) || {}
-    end
+  def self.config
+    return @config if @config
+    @config = YAML.load_file(config_path) || {}
+  end
 end
