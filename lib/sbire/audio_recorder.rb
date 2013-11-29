30e311b2f0c718a8a3d29199374fd481daaf0186
GCorbel <guirec.corbel@gmail.com>
2013-11-29 01:40:15 -0500

    allow more noice

--- lib/sbire/audio_recorder.rb
+++ lib/sbire/audio_recorder.rb
@@ -17,7 +17,7 @@ module Sbire
 
     private
     def record_audio
-      fork { exec "sox -t alsa -r 22050 default #{path}.flac -q silence -l 1 0 1% 1 1.0 1% rate 16k : newfile : restart" }
+      fork { exec "sox -t alsa -r 22050 default #{path}.flac -q silence -l 1 0 2% 1 1.0 2% rate 16k : newfile : restart" }
     end
   end
 end
